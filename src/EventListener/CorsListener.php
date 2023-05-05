4933bbcf41a73a5df57dd10ba5b01a6c8d173bdb
Medaminegdoura <medaminegdoura@gmail.com>
2023-05-05 16:00:23 +0100

    Establishing connection between backend and frontend

---
+++ src/EventListener/CorsListener.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class CorsListener{
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
